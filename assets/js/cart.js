4fadf0d14e4bc7e910921c45179f701e486eb019
khammanh-Tanvilay <manhtanvilay@gmail.com>
2025-07-22 13:20:31 +0700

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -174,13 +174,16 @@ function validateFormOldUI() {
         const feedback = input.nextElementSibling;
         if (feedback && feedback.classList.contains('invalid-feedback')) {
             feedback.style.display = 'none';
+            
         }
         if (!input.value.trim()) {
             isValid = false;
+            console.log("check",isValid);
             input.classList.add('is-invalid');
             if (feedback) feedback.style.display = 'block';
         } else if (input.type === 'email' && !/^\S+@\S+\.\S+$/.test(input.value)) {
             isValid = false;
+            console.log("check3",isValid);
             input.classList.add('is-invalid');
             if (feedback) feedback.style.display = 'block';
         }
